14572fa1dbc0ee2bc7d6049bdb75f3c0dd049c92
Student002 <marnu247@gmail.com>
2014-05-25 10:47:19 -0500

    Completed Assignment 2
    Upload the completed R functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions compute and then cache the inverse of a matrix to reduce 
+##   costly computations. 
 
-## Write a short comment describing this function
+## Function makeCacheMatrix cache the matrix 'x' in memory.
+##   It returns NULL if the cache is empty or or the matrix length have changed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Get the cached matrix if it exists
+    if (exists("CacheMatrixSolved", envir= .GlobalEnv)) {
+        m <- mget("CacheMatrixSolved", envir= .GlobalEnv)
+        l <- mget("CacheMatrixLength", envir= .GlobalEnv)
+        # Check if the matrix has changed
+        if (l != length(x)) {
+            m <- NULL
+        }
+    } else {
+        m <- NULL
+    }
+    
+    # Cache the Solved matrix
+    setinverse <- function(solve) {
+        m <- solve
+        assign("CacheMatrixSolved", m, envir= .GlobalEnv)
+        assign("CacheMatrixLength", length(m), envir= .GlobalEnv)
+    }
+    
+    # Return the solved matrix
+    getinverse <- function() m
+    
+    list(setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve returns the inverse of the matrix 'x'.
+##   It will compute the inverse if it is not cached.
+##   The cache is managed by the makeCacheMatrix function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    # Get the cached matrix
+    m <- makeCacheMatrix(x)$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    # Calculate the inverse of 'x' and cache it
+    m <- solve(x)
+    makeCacheMatrix(x)$setinverse(m)
+    m
 }
+
+
+## My test data ##
+# hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+# h8 <- hilbert(8); h8
+# solve(h8)
+# cacheSolve(h8)
+
+
+
+
